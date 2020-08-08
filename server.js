703b294f47a55f65c0379554bff63068402d63e1
ahmedmeshref <a.meshref@alustudent.com>
2020-08-08 17:40:09 +0200

    Configure app & initialize server

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+// import app dependencies
+const express = require("express"),
+    cors = require("cors"),
+    bodyParser = require("body-parser");
 
-// Require Express to run server and routes
+const port = 8000;
 
-// Start up an instance of app
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+// Initialize an app instance
+const app = express()
+
+/*
+Configuration
+ */
+// Use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
 
-// Setup Server
+/*
+Routes
+ */
+app.get("/weather", function(req, res) {
+    res.send("I am heree");
+})
+
+// Server Setup
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+})
