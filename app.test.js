4f23d3fbd3ea8d05c10c34e4836fd2a6c87f5f0a
ahmedmeshref <a.meshref@alustudent.com>
2020-08-10 22:23:42 +0200

    Add test PostData

--- app.test.js
+++ app.test.js
@@ -8,12 +8,25 @@ beforeAll(() => {
 
 
 test('getData', async () => {
-    const res = await request(app).get('/feeling'),
-        response = {
-            // Date: "7.10.2020",
-            // Temp: 65.28,
-            // Content: "I feel so happy"
-        };
+    const res = await request(app).get('/feeling');
     expect(res.status).toBe(200);
-    console.log(res.body.success);
+    expect(res.body.success).toBeTruthy();
+})
+
+test('sendData', async () => {
+    const data = {
+        date: "7.10.2020",
+        temp: 63.1,
+        content: "Happy"
+    }
+    let postRes = await request(app)
+            .post('/feeling')
+            .send(data)
+    expect(postRes.status).toBe(200);
+    expect(postRes.body.success).toBeTruthy();
+
+    const getRes = await request(app).get('/feeling');
+    expect(getRes.status).toBe(200);
+    expect(getRes.body.success).toBeTruthy();
+    expect(getRes.body.data).toEqual(data);
 })
